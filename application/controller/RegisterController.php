ea04d6e9b9f034696693d1e4a5895510af230649
Ella <ella.leser@siemens.com>
2026-05-11 15:08:54 +0200

    remove user activation hash and activate the user on create

--- application/controller/RegisterController.php
+++ application/controller/RegisterController.php
@@ -47,12 +47,11 @@ class RegisterController extends Controller
     /**
      * Verify user after activation mail link opened
      * @param int $user_id user's id
-     * @param string $user_activation_verification_code user's verification token
      */
-    public function verify($user_id, $user_activation_verification_code)
+    public function verify($user_id)
     {
-        if (isset($user_id) && isset($user_activation_verification_code)) {
-            RegistrationModel::verifyNewUser($user_id, $user_activation_verification_code);
+        if (isset($user_id)) {
+            RegistrationModel::verifyNewUser($user_id);
             $this->View->render('register/verify');
         } else {
             Redirect::to('login/index');
