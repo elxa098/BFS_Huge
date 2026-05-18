83464d57678e9b16e53bea8f175138c2ed6c227d
elxa098 <ell3.leser@gmail.com>
2026-05-18 14:07:58 +0200

    add html code for allUsers tab

---
+++ application/view/user/allUsers.php
@@ -0,0 +1,53 @@
+<table class="overview-table">
+    <thead>
+        <tr>
+            <td>Id</td>
+            <td>Avatar</td>
+            <td>Username</td>
+            <td>User's email</td>
+            <td>Activated ?</td>
+            <td>Link to user's profile</td>
+            <td>Group</td>
+            <td>Suspension Time (days)</td>
+            <td>Soft delete</td>
+        </tr>
+    </thead>
+
+    <?php foreach ($this->users as $user) { ?>
+        <tr class="<?= ($user->user_active == 0 ? 'inactive' : 'active'); ?>">
+
+            <td><?= $user->user_id; ?></td>
+
+            <td class="avatar">
+                <?php if (!empty($user->user_avatar_link)) { ?>
+                    <img src="<?= $user->user_avatar_link; ?>" />
+                <?php } ?>
+            </td>
+
+            <td><?= $user->user_name; ?></td>
+
+            <td><?= $user->user_email; ?></td>
+
+            <td><?= ($user->user_active == 0 ? 'No' : 'Yes'); ?></td>
+
+            <td>
+                <a href="<?= Config::get('URL') . 'profile/showProfile/' . $user->user_id; ?>">
+                    Profile
+                </a>
+            </td>
+
+            <td>
+                <?= htmlspecialchars($user->group_name ?? $user->user_account_type); ?>
+            </td>
+
+            <td>
+                <?= $user->suspension_time ?? '-'; ?>
+            </td>
+
+            <td>
+                <?= ($user->user_deleted ? 'Yes' : 'No'); ?>
+            </td>
+
+        </tr>
+    <?php } ?>
+</table>
