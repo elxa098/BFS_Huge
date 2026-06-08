41cbf957984f833f520f9e21eb09ba53635f5dcc
elxa098 <ell3.leser@gmail.com>
2026-06-08 15:17:48 +0200

    add upload function in pictures controller

--- application/controller/PicturesController.php
+++ application/controller/PicturesController.php
@@ -1,20 +1,16 @@
 <?php
 
-/**
- * PictureController
- * Controlls everything gallery related.
- */
 class PicturesController extends Controller
 {
-    /**
-     * Construct this object by extending the basic Controller class.
-     */
+    private $pathToPictures = ROOT . "/gallery_pictures";
+    private $allowedFileTypes = ['jpg', 'jpeg', 'png', 'pdf'];
+
     public function __construct()
     {
         parent::__construct();
         Auth::checkAuthentication();
-    }
-
+    } 
+    
     /**
      * Show gallery option
      * @return void
@@ -22,5 +18,48 @@ class PicturesController extends Controller
     public function index()
     {
         $this->View->render('pictures/index');
+    }
+
+    public function upload()
+    {
+        if (!isset($_FILES['datei']) || $_FILES['datei']['error'] !== UPLOAD_ERR_OK) {
+            die('Error: No file uploaded.');
+        }
+
+        $file = $_FILES['datei'];
+
+        $fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+        if (!in_array($fileExtension, $this->allowedFileTypes)) {
+            die('File type not allowded.');
+        }
+
+        $currentUser = Session::get('user_id');
+
+        $userFolder = $this->pathToPictures . "/" . $currentUser;
+        if (!is_dir($userFolder)){
+            mkdir($userFolder, 0777, true);
+        }
+
+        $newFilename = uniqid() . '.' . $fileExtension;
+        $destination = $userFolder . "/" . $newFilename;
+
+        if (move_uploaded_file($file['tmp_name'], $destination)) {
+            header('Location: ' . URL . 'pictures');
+            exit;
+        } else {
+            die('Upload failed.');
+        }
+    }
+
+    public function download($pictureId)
+    {
+    }
+
+    public function delete($pictureId)
+    {
+    }
+
+    public function share($pictureId)
+    {
     }
 }
