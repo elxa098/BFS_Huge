dea19115d1c61c61246deb5f5646462a70cca88c
elxa098 <ell3.leser@gmail.com>
2026-06-10 08:11:46 +0200

    added upload to database

--- application/controller/PicturesController.php
+++ application/controller/PicturesController.php
@@ -29,35 +29,42 @@ class PicturesController extends Controller
         }
 
         $file = $_FILES['datei'];
+        $currentUser = Session::get('user_id');
 
+        // Check file extension
         $fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
         if (!in_array($fileExtension, $this->allowedFileTypes)) {
             die('File type not allowded.');
         }
 
-        $currentUser = Session::get('user_id');
+        // Create new unique file name
+        $newFilename = uniqid() . '.' . $fileExtension;
 
+        // Get new picture saving path
         $userFolder = $this->pathToPictures . "/" . $currentUser;
         if (!is_dir($userFolder)){
             mkdir($userFolder, 0755, true);
         }
-
-        $newFilename = uniqid() . '.' . $fileExtension;
         $destination = $userFolder . "/" . $newFilename;
 
+        // Move picture to saving path
         if (move_uploaded_file($file['tmp_name'], $destination)) {
-            Redirect::to('pictures');
-        } else {
+
+            // Upload picture details into database
+            $success = PicturesModel::uploadPicture($currentUser, $newFilename, $file['size'], '');
+            if ($success){
+                Redirect::to('pictures');
+            } 
+            else {
+                die('Database insertion failed');
+            }
+        } 
+        else {
             die('Upload failed.');
         }
 
-        $success = PicturesModel::uploadPicture($currentUser, $newFilename, $file['size'], '');
-        if ($success){
-            Redirect::to('pictures');
-        } else {
-            die('Database insertion failed');
-        }
     }
+
 
     public function download($pictureId)
     {
