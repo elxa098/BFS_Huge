c7b456342c275dcb0601dc713d4c83c83774d2a2
elxa098 <ell3.leser@gmail.com>
2026-06-10 10:01:25 +0200

    fix image placement

--- application/view/pictures/index.php
+++ application/view/pictures/index.php
@@ -1,25 +1,44 @@
 <div class="container">
     <h1>Gallery</h1>
 
+    <style>
+        .panel {
+            width: 100%;
+        }
+
+        .panel.gallery {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 10px;
+        }
+
+        .panel.gallery img {
+            width: 100%;
+            height: 200px;
+            object-fit: cover;
+            display: block;
+            border-radius: 6px;
+        }
+    </style>
+
     <!-- Upload -->
-    <div class="box">
-        <form   method="post" 
-                enctype="multipart/form-data" 
-                action="<?= Config::get('URL'); ?>pictures/upload"
-            >
+    <div class="panel">
+        <form method="post"
+              enctype="multipart/form-data"
+              action="<?= Config::get('URL'); ?>pictures/upload">
             <input type="file" name="datei" accept=".jpg,.png,.pdf">
             <button type="submit">Hochladen</button>
         </form>
     </div> 
-    
+
     <!-- Display Gallery -->
     <h2>Your Pictures</h2>
-    <div class="box">
+    <div class="panel gallery">
         <?php if (!empty($this->data['pictures'])) : ?>
             <?php foreach ($this->data['pictures'] as $picture) : ?>
 
-            <img src="<?= Config::get('URL'); ?>pictures/image/<?= $picture->id ?>">
-            
+                <img src="<?= Config::get('URL'); ?>pictures/image/<?= $picture->id ?>">
+
             <?php endforeach; ?>
         <?php else : ?>
             <p>No pictures uploaded yet.</p>
