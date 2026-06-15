aea4c0c48606dc4314bdc9612d27c8e09020cde0
elxa098 <ell3.leser@gmail.com>
2026-06-15 15:14:10 +0200

    add player selection and game status layout

--- application/view/tictactoe/index.php
+++ application/view/tictactoe/index.php
@@ -2,22 +2,39 @@
     <h1>Tic Tac Toe</h1>
 
     <div class="box">
-        <table class="tictactoe-table">
-            <tr>
-                <td><button data-index="A1"></button></td>
-                <td><button data-index="A2"></button></td>
-                <td><button data-index="A3"></button></td>
-            </tr>
-            <tr>
-                <td><button data-index="B1"></button></td>
-                <td><button data-index="B2"></button></td>
-                <td><button data-index="B3"></button></td>
-            </tr>
-            <tr>
-                <td><button data-index="C1"></button></td>
-                <td><button data-index="C2"></button></td>
-                <td><button data-index="C3"></button></td>
-            </tr>
-        </table>
+        <div class="player-selection">
+            <label>Play with:</label>
+            <select id="opponent">
+                <option value="">Gegner auswählen</option>
+            </select>
+        </div>
+    </div>
+
+    <table class="tictactoe-table">
+        <tr>
+            <td><button id="ttt-a1"></button></td>
+            <td><button id="ttt-a2"></button></td>
+            <td><button id="ttt-a3"></button></td>
+        </tr>
+        <tr>
+            <td><button id="ttt-b1"></button></td>
+            <td><button id="ttt-b2"></button></td>
+            <td><button id="ttt-b3"></button></td>
+        </tr>
+        <tr>
+            <td><button id="ttt-c1"></button></td>
+            <td><button id="ttt-c2"></button></td>
+            <td><button id="ttt-c3"></button></td>
+        </tr>
+    </table>
+
+    <div class="box">
+        <div class="game-status">
+            <span id="statusLabel">Gegner aussuchen um Spiel zu starten.</span>
+        </div>
+
+        <div class="actions">
+            <button id="resetGame">Spiel zurücksetzen</button>
+        </div>
     </div>
 </div>
