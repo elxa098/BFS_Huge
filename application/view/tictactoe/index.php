c409f1869176b0403829dde54f07fb24a49be9d3
elxa098 <ell3.leser@gmail.com>
2026-06-15 14:10:55 +0200

    add basic tic tac toe grid

---
+++ application/view/tictactoe/index.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <h1>Tic Tac Toe</h1>
+
+    <div class="box">
+        <table class="tictactoe-table">
+            <tr>
+                <td><button data-index="A1"></button></td>
+                <td><button data-index="A2"></button></td>
+                <td><button data-index="A3"></button></td>
+            </tr>
+            <tr>
+                <td><button data-index="B1"></button></td>
+                <td><button data-index="B2"></button></td>
+                <td><button data-index="B3"></button></td>
+            </tr>
+            <tr>
+                <td><button data-index="C1"></button></td>
+                <td><button data-index="C2"></button></td>
+                <td><button data-index="C3"></button></td>
+            </tr>
+        </table>
+    </div>
+</div>
